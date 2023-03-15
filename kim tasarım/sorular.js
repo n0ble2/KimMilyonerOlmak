19f25d3e0d7b6b4f3a035c7e6fd3cdb29496da2b
Orhan Tuna <120476288+n0ble2@users.noreply.github.com>
2023-03-15 12:22:39 +0300

    Kim Milyoner Olmak İster
    Jokerleri yapım aşamasında. Html, Css, JavaScript ve Bootstrap kullandım.

---
+++ kim tasarım/sorular.js
@@ -0,0 +1,517 @@
+let sorular = [
+  {
+    //soru bul
+    soru: "Cyberpunk 2077'de hangi karakter ile oynuyoruz.",
+    cevap_a: "Messi",
+    cevap_b: "V",
+    cevap_c: "Keanu Reeves",
+    cevap_d: "Kratos",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "V",
+  }, //b 0
+
+  {
+    soru: "Microsoft'un geliştirdiği ilk oyun hangisidir?",
+    cevap_a: "Halo Combat Evolved",
+    cevap_b: "Gears of War",
+    cevap_c: "Age of Empires",
+    cevap_d: "Ultimate Doom",
+    dogru_cevap: "c",
+    metinsel_dogru_cevap: "Age of Empires",
+  }, //c 1
+
+  {
+    soru: "Microsoft Apple'dan hangi oyun firmasını satın aldı?",
+    cevap_a: "343 Industries",
+    cevap_b: "Santa Monica",
+    cevap_c: "Epic Games",
+    cevap_d: "Bungie",
+    dogru_cevap: "d",
+    metinsel_dogru_cevap: "Bungie",
+  }, //d 2
+
+  {
+    soru: "Halo 3 kaç milyon kopya sattı?",
+    cevap_a: "1 Milyon",
+    cevap_b: "27 Milyon",
+    cevap_c: "7 Milyon",
+    cevap_d: "15 Milyon",
+    dogru_cevap: "d",
+    metinsel_dogru_cevap: "15 Milyon",
+  }, //d 3
+
+  {
+    soru: "Halo'nun Xbox360 sunucuları ne zaman kapatıldı?",
+    cevap_a: "2021",
+    cevap_b: "2018",
+    cevap_c: "2019",
+    cevap_d: "2022",
+    dogru_cevap: "a",
+    metinsel_dogru_cevap: "2021",
+  }, //a 4
+
+  {
+    //soru bul
+    soru: "Fifa 2023'ün kapağında hangi oyuncu var?",
+    cevap_a: "Messi",
+    cevap_b: "Mbappe",
+    cevap_c: "Ronaldo",
+    cevap_d: "John",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "Mbappe",
+  }, //b 5
+
+  {
+    //soru bul
+    soru: "2001 yılında hangi konsol oyunu GOTY kazanmıştır?",
+    cevap_a: "Diablo 2001",
+    cevap_b: "Microsoft Flight Simulator 2002",
+    cevap_c: "Max Payne",
+    cevap_d: "Halo Combat Evolved",
+    dogru_cevap: "d",
+    metinsel_dogru_cevap: "Halo Combat Evolved",
+  }, //6
+
+  {
+    //soru bul 6
+    soru: "İlk Need for Speed oyunu ne zaman çıktı?",
+    cevap_a: "1994",
+    cevap_b: "1996",
+    cevap_c: "1999",
+    cevap_d: "1992",
+    dogru_cevap: "a",
+    metinsel_dogru_cevap: "1994",
+  }, // 7
+
+  {
+    //soru bul
+    soru: "Günümüze kadar yaklaşık ne kadar Ps2 satıldı?",
+    cevap_a: "90 Milyon",
+    cevap_b: "150 Milyon",
+    cevap_c: "181 Milyon",
+    cevap_d: "31 Milyon",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "150 Milyon", //b
+  }, //8
+
+  {
+    //soru bul
+    soru: "2022 yılında hangi oyun GOTY kazandı?",
+    cevap_a: "Halo 4",
+    cevap_b: "Zula",
+    cevap_c: "Elden Ring",
+    cevap_d: "GTA 6",
+    dogru_cevap: "c",
+    metinsel_dogru_cevap: "Elden Ring", //c
+  }, //9
+
+  {
+    //soru bul
+    soru: "Destiny ve Halo'da hangi oyun motoru kullanılmıştır?",
+    cevap_a: "Red Engine",
+    cevap_b: "Blam Engine",
+    cevap_c: "Zero",
+    cevap_d: "Unreal Engine",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "Blam Engine",
+  }, //10
+
+  {
+    soru: "Halo'da hangi oyun motoru kullanılmıştır?",
+    cevap_a: "Red Engine",
+    cevap_b: "Unity Game Engine",
+    cevap_c: "SlipSpace Engine",
+    cevap_d: "Zero",
+    dogru_cevap: "c",
+    metinsel_dogru_cevap: "SlipSpace Engine",
+  }, //11
+];
+
+let sorun = [
+  {
+    soru: "Eski Türkler hangi inanca sahiptiler",
+    cevap_a: "İslamiyet",
+    cevap_b: "Gök Tengri",
+    cevap_c: "Makarna Canavarı",
+    cevap_d: "YUVHA",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "Gök Tengri",
+  }, //b 0
+
+  {
+    soru: "T.C.'nin açılımı nedir?",
+    cevap_a: "Türkiye",
+    cevap_b: "Trabzon",
+    cevap_c: "Türkiye Cumhuriyeti",
+    cevap_d: "Temiz Cam",
+    dogru_cevap: "c",
+    metinsel_dogru_cevap: "Türkiye Cumhuriyeti",
+  }, //c 1
+
+  {
+    soru: "Türkiye'nin başkenti neresidir?",
+    cevap_a: "Rize",
+    cevap_b: "Konya",
+    cevap_c: "İstanbul",
+    cevap_d: "Ankara",
+    dogru_cevap: "d",
+    metinsel_dogru_cevap: "Ankara",
+  }, //d 2
+
+  {
+    soru: "Osmanlı'nın ilk padişahı kimdir?",
+    cevap_a: "Ertuğrul Gazi",
+    cevap_b: "Orhan Gazi",
+    cevap_c: "Kenan Komutan",
+    cevap_d: "Osman Gazi",
+    dogru_cevap: "d",
+    metinsel_dogru_cevap: "Osman Gazi",
+  }, //d 3
+
+  {
+    soru: "Süleyman Şah günümüz haritasında nerede doğdu?",
+    cevap_a: "İran",
+    cevap_b: "Yunanistan",
+    cevap_c: "Rusya",
+    cevap_d: "Papua Yeni Gine",
+    dogru_cevap: "a",
+    metinsel_dogru_cevap: "İran",
+  }, //a 4
+
+  {
+    soru: "Cumhuriyet ne zaman ilan edildi?",
+    cevap_a: "1921",
+    cevap_b: "1923",
+    cevap_c: "1941",
+    cevap_d: "2077",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "1923",
+  }, //b 5
+];
+
+let srs = [
+  {
+    soru: "Amerikanın 45. başkanı kimdir?",
+    cevap_a: "Barack Obama",
+    cevap_b: "Donald Trump",
+    cevap_c: "Recep Tayyip Erdoğan",
+    cevap_d: "Joe Biden",
+    dogru_cevap: "b",
+    metinsel_dogru_cevap: "Donald Trump",
+  }, // b 8
+  {
+    soru: "Yedi renkten oluşan gökkuşağının ortasındaki renk hangisidir?",
+    cevap_a: "Mavi",
+    cevap_b: "Sarı",
+    cevap_c: "Yeşil",
+    cevap_d: "Kırmızı",
+    dogru_cevap: "c",
+    metinsel_dogru_cevap: "Yeşil",
+  }, // c 9
+  {
+    soru: "OS/360'ı geliştiren sirketin adı aşağıdakilerden hangisidir?",
+    cevap_a: "IBM",
+    cevap_b: "Microsoft",
+    cevap_c: "Apple",
+    cevap_d: "Sony",
+    dogru_cevap: "a",
+    metinsel_dogru_cevap: "IBM",
+  },
+];
+
+//sorular[0].dogru_cevap = sorun[0].dogru_cevap; //1
+//sorular[1].dogru_cevap = sorun[1].dogru_cevap; //2
+//sorular[2].dogru_cevap = sorun[2].dogru_cevap; //3
+//sorular[3].dogru_cevap = sorun[3].dogru_cevap; //4
+//sorular[4].dogru_cevap = sorun[4].dogru_cevap; //5
+//sorular[5].dogru_cevap = sorun[5].dogru_cevap; //6
+
+/*sorular[0] = sorun[0]; //1
+sorular[1] = sorun[1]; //2
+sorular[2] = sorun[2]; //3
+sorular[3] = sorun[3]; //4
+sorular[4] = sorun[4]; //5
+sorular[5] = sorun[5]; //6 --
+sorular[6] = sorular[6]; //7--
+sorular[7] = sorular[7]; //8--
+sorular[8] = srs[0]; //9
+sorular[9] = srs[1]; //10
+sorular[10] = sorular[10]; //11 */
+
+/* shuffle(sorular_1); */
+
+/*for (let soru_ine = 0; soru_ine < 1; soru_ine++) {
+  console.log(sorular_1[soru_ine].soru);
+  console.log(sorular_1[soru_ine].cevap_a);
+  console.log(sorular_1[soru_ine].cevap_b);
+  console.log(sorular_1[soru_ine].cevap_c);
+  console.log(sorular_1[soru_ine].cevap_d);
+}
+
+for (let soru_inex = 0; soru_inex < 1; soru_inex++) {
+  console.log(sorular[soru_inex].soru);
+  console.log(sorular[soru_inex].cevap_a);
+  console.log(sorular[soru_inex].cevap_b);
+  console.log(sorular[soru_inex].cevap_c);
+  console.log(sorular[soru_inex].cevap_d);
+} */
+
+let param = [
+  {
+    ilk: "0 TL",
+  },
+  {
+    ilk: "2000 TL",
+  },
+  {
+    ilk: "3000 TL",
+  },
+  {
+    ilk: "4000 TL",
+  },
+  {
+    ilk: "5000 TL",
+  },
+  {
+    ilk: "6000 TL",
+  },
+  {
+    ilk: "7000 TL",
+  },
+  {
+    ilk: "8000 TL",
+  },
+  {
+    ilk: "9000 TL",
+  },
+  {
+    ilk: "10.000 TL",
+  },
+  {
+    ilk: "100.000 TL",
+  },
+  {
+    ilk: "100.020 TL",
+  },
+];
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+shuffle(sorular);
+shuffle(sorun);
+shuffle(srs);
+
+// Used like so
+
+let soru_inex = -1;
+let para_inex = -1;
+let zort = -1;
+let zorti = -1;
+let cift_cevap = false;
+let cevap_toplam_verdigi = 0;
+
+function sonraki() {
+  cevap_toplam_verdigi = 0;
+  cift_cevap = false;
+  document.getElementById("tekrar").style.background = "black";
+  soru_inex++;
+  para_inex++;
+  /*
+  const cevaplar = [
+    document.getElementById("cvp_a"),
+    document.getElementById("cvp_b"),
+    document.getElementById("cvp_c"),
+    document.getElementById("cvp_d"),
+  ];
+  for (const cevap of cevaplar) {
+    cevap.style.display = "inline-block";
+  }
+  */
+  document.getElementById("index").innerHTML = soru_inex + 1;
+  document.getElementById("demo").innerHTML = sorular[soru_inex].soru;
+  document.getElementById("cvp_1").innerHTML = sorular[soru_inex].cevap_a;
+  document.getElementById("cvp_2").innerHTML = sorular[soru_inex].cevap_b;
+  document.getElementById("cvp_3").innerHTML = sorular[soru_inex].cevap_c;
+  document.getElementById("cvp_4").innerHTML = sorular[soru_inex].cevap_d;
+  document.getElementById("par").innerHTML = param[para_inex].ilk;
+
+  if (sorular[soru_inex] == sorular[0]) {
+    zort++;
+    document.body.style.backgroundColor = "red";
+    document.getElementById("demo").innerHTML = sorun[zort].soru;
+    document.getElementById("cvp_1").innerHTML = sorun[zort].cevap_a;
+    document.getElementById("cvp_2").innerHTML = sorun[zort].cevap_b;
+    document.getElementById("cvp_3").innerHTML = sorun[zort].cevap_c;
+    document.getElementById("cvp_4").innerHTML = sorun[zort].cevap_d;
+  }
+  if (sorular[soru_inex] == sorular[1]) {
+    zort++;
+    document.body.style.backgroundColor = "black";
+    document.getElementById("demo").innerHTML = sorun[zort].soru;
+    document.getElementById("cvp_1").innerHTML = sorun[zort].cevap_a;
+    document.getElementById("cvp_2").innerHTML = sorun[zort].cevap_b;
+    document.getElementById("cvp_3").innerHTML = sorun[zort].cevap_c;
+    document.getElementById("cvp_4").innerHTML = sorun[zort].cevap_d;
+  }
+  if (sorular[soru_inex] == sorular[2]) {
+    zort++;
+    document.body.style.backgroundColor = "yellow";
+    document.getElementById("demo").innerHTML = sorun[zort].soru;
+    document.getElementById("cvp_1").innerHTML = sorun[zort].cevap_a;
+    document.getElementById("cvp_2").innerHTML = sorun[zort].cevap_b;
+    document.getElementById("cvp_3").innerHTML = sorun[zort].cevap_c;
+    document.getElementById("cvp_4").innerHTML = sorun[zort].cevap_d;
+  }
+  if (sorular[soru_inex] == sorular[8]) {
+    zorti++;
+    document.getElementById("demo").innerHTML = srs[zorti].soru;
+    document.getElementById("cvp_1").innerHTML = srs[zorti].cevap_a;
+    document.getElementById("cvp_2").innerHTML = srs[zorti].cevap_b;
+    document.getElementById("cvp_3").innerHTML = srs[zorti].cevap_c;
+    document.getElementById("cvp_4").innerHTML = srs[zorti].cevap_d;
+  }
+  if (sorular[soru_inex] == sorular[9]) {
+    zorti++;
+    document.getElementById("demo").innerHTML = srs[zorti].soru;
+    document.getElementById("cvp_1").innerHTML = srs[zorti].cevap_a;
+    document.getElementById("cvp_2").innerHTML = srs[zorti].cevap_b;
+    document.getElementById("cvp_3").innerHTML = srs[zorti].cevap_c;
+    document.getElementById("cvp_4").innerHTML = srs[zorti].cevap_d;
+  }
+  document.getElementById("questler").innerHTML =
+    document.getElementById("demo").innerHTML;
+}
+
+zorti = 0;
+let veri = 0;
+let coin = 1;
+let lb = 0;
+let dodge = 0;
+let online = 0;
+
+function cevap(cvp) {
+  console.log(srs[zorti].metinsel_dogru_cevap);
+
+  if (
+    cvp == sorun[zort].metinsel_dogru_cevap ||
+    cvp == sorular[soru_inex].metinsel_dogru_cevap ||
+    cvp == srs[zorti].metinsel_dogru_cevap
+  ) {
+    saniye = clearInterval(saniyeM());
+    $("#next-quest").modal("show");
+    veri++;
+    coin++;
+    lb++;
+    dodge++;
+    /*document.getElementById("questler").innerHTML = sorular[veri].soru; */
+    document.getElementById("mani").innerHTML = param[coin].ilk;
+    document.getElementById("part").innerHTML = lb + 01;
+    document.getElementById("mavi").innerHTML = param[dodge].ilk;
+    document.getElementById("aldim").innerHTML = param[dodge].ilk;
+  } else {
+    $("#olmadi").modal("show");
+    document.getElementById("rt").innerHTML = `${lb + 01}. SORU SENİ ELEDİ`;
+  }
+  if (sorular[soru_inex] == sorular[9]) {
+    $("#bitti_umr").modal("show");
+  }
+  sonraki();
+
+  //document.getElementById("").style.background = "green"; dogru cevabın yeşil olması için en son yap burayı
+  // burası önemli
+}
+
+//
+function halo() {
+  $("#para_al").modal("show"); //yönlendirme komutu
+}
+
+//soru süresi
+function fark() {
+  saniye = 45;
+  document.getElementById("saniye").style.color = "white";
+}
+
+document.getElementById("miktar").innerHTML = "KAZANILAN PARA";
+
+var sure;
+var saniye = clearInterval(saniyeM());
+function saniyeM() {
+  saniye--;
+  if (saniye <= -1) {
+    document.getElementById("lt").innerHTML = `${lb + 1}. SORUDA ELENDİNİZ.`; // OP LA
+    $("#no-time").modal("show");
+  }
+  document.getElementById("saniye").innerHTML = saniye;
+
+  if (23 >= saniye) {
+    document.getElementById("saniye").style.color = "yellow";
+  }
+}
+var sure = window.setInterval(saniyeM, 1000);
+
+function exit() {
+  if (sorular[soru_inex] == sorular[0]) saniye = 45;
+}
+
+function paralar() {
+  $("#paraModal").modal("show");
+}
+
+function f5() {
+  location.reload();
+}
+
+sonraki();
+
+/*
+function yarim() {
+  const dogru_cevap = sorular[soru_inex].metinsel_dogru_cevap;
+  const cevaplar = [
+    document.getElementById("cvp_a"),
+    document.getElementById("cvp_b"),
+    document.getElementById("cvp_c"),
+    document.getElementById("cvp_d"),
+  ];
+  let sayac = 0;
+  for (const cevap of cevaplar) {
+    const cvp = cevap.querySelector("p").textContent;
+    if (cvp == dogru_cevap) {
+      continue;
+    }
+    if (sayac >= 2) {
+      break;
+    }
+    sayac++;
+    cevap.style.display = "none";
+  }
+}
+*/
+document.getElementById("sani").innerHTML;
+function tekrar() {
+  cift_cevap = true;
+  document.getElementById("tekrar").style.background = "green";
+  // if (window.prompt("a - b - c - d") == sorular[soru_inex].dogru_cevap) {
+  //   window.alert("cevabınız doğru");
+  // } else {
+  //   window.alert("cevabınız yanlış, yazıka kafa");
+  // }
+}
